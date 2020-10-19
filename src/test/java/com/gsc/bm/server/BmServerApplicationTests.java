366aab61aa949194e5c0c0446fff8864f05e5345
gesucca <gesucca.official@gmail.com>
2020-10-19 15:17:21 +0200

    init commit

---
+++ src/test/java/com/gsc/bm/server/BmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsc.bm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
