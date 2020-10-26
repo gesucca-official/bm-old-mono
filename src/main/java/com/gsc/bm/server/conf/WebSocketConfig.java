12b58317f9c1f73fc3f18f7932c148b3ea0425c3
gesucca <gesucca.official@gmail.com>
2020-10-26 12:47:08 +0100

    new client version with hidden info client side (that's bad)

--- src/main/java/com/gsc/bm/server/conf/WebSocketConfig.java
+++ src/main/java/com/gsc/bm/server/conf/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/bm-server")
-                .setAllowedOrigins("http://localhost:4200") //TODO only for angular
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
