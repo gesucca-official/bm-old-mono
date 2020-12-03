006b0353886900eec3d6a4e5f3b9542c89b47429
gesucca <gesucca.official@gmail.com>
2020-12-03 16:44:24 +0100

    opponents and tracer code for drag n drop

---
+++ src/main/ts/src/app/client-phaser/scnenes/battle/model/ui-opponent.ts
@@ -0,0 +1,56 @@
+import {PhaserSettingsService} from "../../../phaser-settings.service";
+import {GameService} from "../../../../service/game.service";
+import {Player} from "../../../../model/player";
+
+export class UI_Opponent {
+
+  private readonly container: Phaser.GameObjects.Container;
+
+  getContainer(): Phaser.GameObjects.Container {
+    return this.container;
+  }
+
+  private settingsService: PhaserSettingsService;
+  private gameService: GameService;
+
+  constructor(scene: Phaser.Scene, model: Player, index: number) {
+    this.settingsService = window['settingsService'];
+    this.gameService = window['gameService'];
+
+    // TODO dynamically load this
+    const character = scene.add.image(0, 0, model.character.name = 'Spazienzio de la Ucciso' ? 'spazienzio' : 'tossico')
+      .setDisplaySize(this.getOppoW(), this.getOppoH());
+    this.container = scene.add.container(
+      this.getOppoX(character.displayWidth, index), this.getOppoY(character.displayHeight),
+      [character]);
+
+    this.container.setSize(character.displayWidth, character.displayHeight);
+    this.container.setInteractive()
+
+    UI_Opponent.setDropZone(scene, this.container, model);
+
+    scene.input.enableDebug(this.container)
+  }
+
+  private static setDropZone(scene: Phaser.Scene, container: Phaser.GameObjects.Container, model: Player) {
+    scene.add.zone(container.x, container.y, container.displayWidth, container.displayHeight)
+      .setRectangleDropZone(container.displayWidth, container.displayHeight)
+      .setData(model.playerId);
+  }
+
+  private getOppoX(templateWidth: number, index: number): number {
+    return (this.settingsService.getScreenWidth() * 0.05) + (index * this.settingsService.scaleForMin(130)) + (templateWidth / 2);
+  }
+
+  private getOppoY(templateHeight: number): number {
+    return (this.settingsService.getScreenHeight() * 0.05) + (templateHeight / 2)
+  }
+
+  private getOppoW(): number {
+    return this.settingsService.scaleForWidth(300)
+  }
+
+  private getOppoH(): number {
+    return this.settingsService.scaleForHeight(300)
+  }
+}
